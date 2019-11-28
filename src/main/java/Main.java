2ac3aef889a026c75aefb70dd23950a920c1f92a
Mykola Sopushynskyi <sopushynskyymykola@gmail.com>
2019-11-28 14:21:34 -0500

    MS: Added comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println(reverse("12345"));
     }
 
+    //reverse string
     public static String reverse(String str) {
         if (str == null)
             return null;
@@ -23,8 +24,9 @@ public class Main {
         return result.toString();
     }
 
+    //find pairs
     public static List<Pair<Integer, Integer>> findPairs(int[] a, int[] b, int sum) {
-        // validation
+        // input validation
         if (a == null)
             return null;
         if (b == null)
@@ -37,10 +39,13 @@ public class Main {
         List<Pair<Integer, Integer>> result = new ArrayList<>();
         HashMap<Integer, Integer> search = new HashMap<>();
 
+        // add the array1 elements as map keys
         for (int i = 0; i < length; i++) {
             search.put(a[i], i);
         }
 
+        // check if array2 elements has a pair inside
+        // hashmap keys(array1 elements) which form the sum we need
         for (int i = 0; i < length; i++) {
             if (search.containsKey(sum - b[i])) {
                 result.add(Pair.of(sum - b[i], b[i]));
