70b62b651a36560b2545716f09bb602bcf6879e6
Mykola Sopushynskyi <sopushynskyymykola@gmail.com>
2019-11-28 14:08:30 -0500

    MS: Added search pairs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
-import jdk.internal.net.http.common.Pair;
-import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 
@@ -8,20 +11,43 @@ public class Main {
     }
 
     public static String reverse(String str) {
-        if(str == null)
+        if (str == null)
             return null;
 
         StringBuilder result = new StringBuilder();
 
-        for(int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             result.append(str.charAt(str.length() - i - 1));
         }
 
         return result.toString();
     }
 
-    public static Pair<Integer, Integer>[] arrayFind(int [] a, int [] b, int sum ) {
-        return null;
+    public static List<Pair<Integer, Integer>> findPairs(int[] a, int[] b, int sum) {
+        // validation
+        if (a == null)
+            return null;
+        if (b == null)
+            return null;
+        if (a.length != b.length)
+            return null;
+
+        int length = a.length;
+
+        List<Pair<Integer, Integer>> result = new ArrayList<>();
+        HashMap<Integer, Integer> search = new HashMap<>();
+
+        for (int i = 0; i < length; i++) {
+            search.put(a[i], i);
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (search.containsKey(sum - b[i])) {
+                result.add(Pair.of(sum - b[i], b[i]));
+            }
+        }
+
+        return result;
     }
 }
 
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,10 @@
-import jdk.internal.net.http.common.Pair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
-import org.apache.commons.lang3.ArrayUtils;
-import static org.junit.Assert.*;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 //TODO mailto:yaroslav.tanko@globallogic.com
 
@@ -16,12 +19,50 @@ public class MainTest {
 
     @Test
     public void arrayTest() {
-        int [] a;
-        int [] b;
-        int SUM = 100;
+        int[] A;
+        int[] B;
+        int EXPECTED_SUM = 100;
+        List<Pair<Integer, Integer>> result;
 
-        assertArrayEquals(new int[][]{{50}, {50}}, Main.arrayFind(new int[]{50}, new int[] {50}, SUM));
-        assertArrayEquals(new int[][]{{}}, Main.arrayFind(new int[]{51}, new int[] {50}, SUM));
+        // test corner cases
+        A = new int[]{50, 100};
+        B = new int[]{50};
+        assertNull(Main.findPairs(null, B, EXPECTED_SUM));
+        assertNull(Main.findPairs(A, B, EXPECTED_SUM));
+        assertNull(Main.findPairs(A, null, EXPECTED_SUM));
+        assertNull(Main.findPairs(null, null, EXPECTED_SUM));
+
+        // test single pair
+        A = new int[]{50};
+        B = new int[]{50};
+        result = Main.findPairs(A, B, EXPECTED_SUM);
+        assertResult(EXPECTED_SUM, 1, result);
+
+        // test single pair
+        A = new int[]{100};
+        B = new int[]{0};
+        result = Main.findPairs(A, B, EXPECTED_SUM);
+        assertResult(EXPECTED_SUM, 1, result);
+
+        // test single pair
+        A = new int[]{200};
+        B = new int[]{-100};
+        result = Main.findPairs(A, B, EXPECTED_SUM);
+        assertResult(EXPECTED_SUM, 1, result);
+
+        // test multiple pairs
+        A = new int[]{37, 63, 45, 20};
+        B = new int[]{80, 55, 50, 63};
+        result = Main.findPairs(A, B, EXPECTED_SUM);
+        assertResult(EXPECTED_SUM, 3, result);
     }
 
+    private void assertResult(int expectedSum, int expectedSize, List<Pair<Integer, Integer>> result) {
+        assertEquals(expectedSize, result.size());
+        result.forEach(p -> {
+            assertEquals(expectedSum, p.getLeft() + p.getRight());
+            System.out.print(String.format("pair %4s %4s\n", p.getLeft(), p.getRight()));
+        });
+        System.out.println("");
+    }
 }
